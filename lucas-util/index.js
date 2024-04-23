cb935216823225bb2284c2b9b856e811b732dd6b
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-04-23 21:37:25 +0900

    add: JS - Node Library Test Files ADD

---
+++ lucas-util/index.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+    return a + b;
+}
+
+function greet() {
+    console.log("Hello! World!");
+}
+
+module.exports = {add, greet};
---
+++ test-dir/index.js
@@ -0,0 +1,4 @@
+const {add, greet} = require('@lucas-util/lucas-util');
+
+console.log(add(1, 2)); // 3
+greet(); // Hello! World!
