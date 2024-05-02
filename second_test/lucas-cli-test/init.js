0a94428c2d65f513ecfb9a1b6921ec8fdcea898a
seokjun <swings@unvus.com>
2024-05-02 10:38:57 +0900

    second-test - cli deploy success v1.0.5 : Make File, Make Dir success (window, mac)

--- second_test/lucas-cli-test/init.js
+++ second_test/lucas-cli-test/init.js
@@ -1,16 +1,51 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
+
 (() => {
-    console.log('[lucas-cli] RUN.');
+    console.log('[lucas-cli-test] RUN.');
 
     const opt = process.argv[2];
-    console.log("process.argv[0]: " + process.argv[2]); // Options
+    console.log("process.argv[2] = OPTIONS : " + process.argv[2]); // Options
 
-    if(opt !== 'init' || opt === '' || opt === null) {
+    if (opt !== 'init' || opt === '' || opt === null) {
         console.log("Error: not required Command");
         return;
     }
 
-    console.log("Success: Command Run!!!");
+    // current directory path get
+    const currentDir = process.cwd();
 
+    // genie-config directory path generate.
+    const genieConfDir = path.join(currentDir, 'genie-config');
+
+    // Check if directory already exists
+    if (!fs.existsSync(genieConfDir)) {
+        // If genie-config directory is not exist make directory
+        fs.mkdirSync(genieConfDir);
+        console.log('make "genie-config" Directory!!');
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+        // genie.conf.js file code write
+        const genieConfCode = `
+const config = {
+    appName: genie.conf.js',
+    version: '1.0.0'
+};`
+;
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // genie.conf.js make file path
+        const genieConfFile = path.join(genieConfDir, 'genie.conf.js');
+
+        // genie.conf.js 파일을 생성합니다.
+        fs.writeFileSync(genieConfFile, genieConfCode);
+        console.log('make "genie.conf.js" file!!');
+    } else {
+        console.log('genie-config directory already exists.');
+        return;
+    }
+
+    console.log("Success: Directory and make File Success!!");
 })();
