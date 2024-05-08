786d815686929f2b3fbcecd8ddbe454931433459
seokjun <swings@unvus.com>
2024-05-08 15:40:37 +0900

    conf -> desc 작성

--- sample/genie.conf-sample.js
+++ sample/genie.conf-sample.js
@@ -1,22 +1,33 @@
 /**
- * 
+ * genie.conf-full-sample.js
  * @param {*} userCfg 코드지니 화면에서 사용자가 설정한 내용이 담겨져 있습니다.
  * @param {*} path node:path 모듈입니다.
  * @param {*} _ lodash 입니다.
+ * @param {*} crypto node:crypto 모듈입니다.
  * @returns baseConfig, globalEngines, moduleCallback 세가지를 반환합니다.
  */
 module.exports.getConfig = (userCfg, path, _, crypto) => {
+
   /**
    * 참고 
-   * userCfg 는 다음과 같은 구조로 되어 있습니다.
-   * 어쩌구 저쩌구
+   * userCfg 는 다음과 같은 내용들을 포함하고 있습니다.
+   * - (Project 정보, DB 설정 정보, 코드지니 설정 정보)
    */
 
   // =============================================================================== 1. baseConfig
-  // baseConfig 는 현재 프로젝트에 대한 설명을 코드지니에게 알려주기 위한 Json 오브젝트입니다.
-  // baseConfig 에는 다음과 같은 항목들을 기술 할 수 있습니다.
 
-  const baseConfig = {
+    /**
+     * baseConfig 는 현재 프로젝트에 대한 설명을 코드지니에게 알려주기 위한 Json 오브젝트입니다.
+     * baseConfig 에는 다음과 같은 항목들을 기술 할 수 있습니다.
+     *   - 프로젝트 생성 빌더
+     *   - git repository 이름
+     *   - 프로젝트 이름
+     *   - 기본 패키지
+     *   - 테이블 접두어
+     *   - 모듈 정보
+     *   - 기타 추가 정보
+     */
+    const baseConfig = {
     builderName: 'Test Project Builder',
     gitName: 'sampleGit',
     name: 'sampleName',
@@ -32,8 +43,11 @@ module.exports.getConfig = (userCfg, path, _, crypto) => {
   };
 
   // =============================================================================== 2. globalEngines
-  // globalEngines 은 프로젝트 루트(최상위) 기준으로 코드지니 프로젝트 빌더가 작업할 내용을 전달해 줍니다.
 
+  /**
+   * globalEngines 은 프로젝트 루트(최상위) 기준으로 코드지니 프로젝트 빌더가 작업할 내용을 전달해 줍니다.
+   * globalEngines 내부에는 DB 설정 정보 및, 최상위 설정 디렉토리 Copy 수행시 필요한 정보들이 포함되어 있습니다.
+   */
   const globalEngines = [];
   const engineRootCopy = {
     engine: 'CopyAndReplaceContent',
@@ -45,15 +59,15 @@ module.exports.getConfig = (userCfg, path, _, crypto) => {
       {from: 'sample-parent', to: `${userCfg.id}-parent`},
       {from: 'sample-core', to: `${userCfg.id}-core`},
       {from: 'sample', to: userCfg.id},
-      {from: 'org.mariadb.jdbc:mariadb-java-client', to: ''},
+      {from: 'org.mariadb.jdbc:mariadb-java-client', to: ''}, // DB Driver 설정(Sample)
       {from: `DB_DRIVER: '1.0.0'`, to: `DB_DRIVER: ''`},
       {
         regex: '',
-        to: maker.getToModules(userCfg)
+        to: 'Custom Module 설정(userCfg.modules)에 따른 추가 설정이 필요합니다.'
       },
       {
         regex: '',
-        to: maker.getToSubs(userCfg)
+        to: 'Custom Module 설정(userCfg.modules)에 따른 추가 설정이 필요합니다. (Version, Description, etc.)'
       }
     ]
   };
@@ -78,6 +92,7 @@ module.exports.getConfig = (userCfg, path, _, crypto) => {
   globalEngines.push(engineGenieCopy);
 
   // .genie 설정 디렉토리 파일명 변경
+  // 사용자가 설정한 테이블 접두어로 .genie/ 디렉토리 내에 있는 파일명을 변경합니다.
   const engineGenieRename = {
     engine: 'RenameFile',
     path : '.genie',
@@ -90,6 +105,7 @@ module.exports.getConfig = (userCfg, path, _, crypto) => {
   globalEngines.push(engineGenieRename);
 
   // genie-sql 설정 디렉토리 파일 카피
+  // - genie-sql/ 디렉토리는 설정 초기 필요한 Init SQL 파일들을 포함합니다.
   const engineGenieSqlCopy = {
     engine: 'CopyAndReplaceContent',
     path : path.join('genie-sql', 'DB Name 작성'),
@@ -107,13 +123,19 @@ module.exports.getConfig = (userCfg, path, _, crypto) => {
   // 코드지니에서 사용자가 선택한 모듈별로 이 메소드가 호출됩니다.
   const moduleCallback = (md) => {
     /**
-     * md 는 이렇게 생겼습니다.
-     * 어쩌구 저쩌구
+     * md 는 사용자가 선택한 모듈 정보가 담겨져 있습니다.
+     * 코드지니에서 설정한 내용을 기반으로, 해당 모듈에 대한 설정을 반환합니다.
+     * 또한 해당 moduleCallback 에서 설정한 내용을 반환함에 따라, 각 모듈별 상세 설정이 가능합니다.
+     *
+     * 특정 경로의 디렉토리 및 파일들을 생성에서 제외시킬 수 있으며, 해당 내용을 반환하여 코드지니에게 전달합니다.
+     *
+     * Ex) Redis 사용 X 시, 해당 모듈의 Redis 설정 파일을 제외할 수 있습니다.
      */
     const moduleCamel = _.camelCase(md.id);
-    const moduleName = _.upperFirst(moduleCamel); // MobileWeb
-    const typeName = _.upperFirst(_.camelCase(md.type)); // Channel
-    const engines = [];
+    const moduleName = _.upperFirst(moduleCamel);
+    const typeName = _.upperFirst(_.camelCase(md.type));
+    const engines = []; // moduleCallback 내부에 설정한 내용들은 해당 변수에 담아서 반환합니다.
+
     // copy module directory
     const moduleEngine = {
       engine: 'CopyAndReplaceContent',
@@ -131,6 +153,7 @@ module.exports.getConfig = (userCfg, path, _, crypto) => {
         {from: `${typeName}Properties`, to: `${moduleName}Properties`},
         {from: `${md.type}Properties`, to: `${moduleCamel}Properties`},
         {from: '"sample"', to: `"${userCfg.id}"`},
+        {from: '@ComponentScan({"com.my"})', to: `@ComponentScan({"com.my", "${userCfg.basePackage}"})`},
       ],
       renames: [
         {from: `sample${typeName}`, to: `${userCfg.idName}${moduleName}`},
@@ -138,11 +161,18 @@ module.exports.getConfig = (userCfg, path, _, crypto) => {
     }
     engines.push(moduleEngine);
 
-    engines.push(Object.assign({}, renameBase, {javaBase: path.join(mdName, 'src', 'test', 'java')} ));
+    // 각 모듈의 Back-end 소스 경로를 추가합니다.
+    let mdName = md.id;
+    if(md.type === 'admin' || md.type === 'test-front') {
+        mdName += path.sep + 'backend';
+    }
+
+    engines.push(Object.assign({}, {javaBase: path.join(mdName, 'src', 'test', 'java')} ));
 
     return engines;
   }
 
+  // 최종적으로 설정 정보를 반환합니다.
   return {
     baseConfig, globalEngines, moduleCallback
   }
